5fae01f1d2ac65f3c287acff730862a2daa16d6d
bouxtehouve <vicversa@wanadoo.fr>
2014-02-20 19:06:42 +0100

    Added temp commenting Network.h and network debugger for main

--- Programme/Programme/Network.h
+++ Programme/Programme/Network.h
@@ -6,15 +6,24 @@ class Network{
 	typedef std::vector<Neuron> Layer;
 public:
 	//Network(const std::vector<unsigned> &architecture, const TransfertFunction &transfertFunction,Parameters p);
-	Network(double eta, double alpha, Transfert choice, std::vector<unsigned> &architecture);
+	Network(double eta, double alpha, Transfert choice, std::vector<unsigned> &architecture); //Constructeur
 	void feedForwardNetwork(const std::vector<double> &inputVals);
+	//Pour une valeur d'input donnee, fait progresser l'information jusqu'a la derniere couche
 	void backPropNetwork(const std::vector<double> &targetVals);
+	//Effectue une retropropagation du gradient selon les valeurs que l'on aurait voulu avoir pour un input sur la couche de sortie
+	//Je te conseille de prendre pour targetVal le vecteur (0,0,...1,0,...,0) de longueur 10, le 1 est pour le chiffre de 0 à 9 correspondant
+	// J'espère que cette fonction fait bien ce qu'on dit !
 	void getResultsNetwork(std::vector<double> &resultVals);
+	//resultVals est modifie pour valoir le vecteur de ce que renvoie le réseau sur la dernière couche ( étant donné les derniers inputs feedforward )
 	int interpretResults(std::vector<double> &resultVals);
+	// Renvoie le digit correspondant a getResultsNetwork ( en gros, si la i-ème neurone de la dernière couche renvoie la valeur max, alors le réseau a reconnu comme un i l'input
 
 	void training(const std::vector<std::vector<double> > &inputValsTab, const std::vector<std::vector<double> > &outputValsTab);
+	// répétition de singletraining sur un vecteur d'input/output
 	void singleTraining(const std::vector<double> &inputVals, const std::vector<double> &outputVals);
+	// prend en entrée un vecteur input, en sortie l'output voulu ( 0,...1,...0 ) donc, et améliore le réseau par rétropropagation
 	void singleTesting(const std::vector<double> &inputVals, int &wanted_digit);
+	// (pour la gui) une fois le réseau entraîné, regarde pour un input donné si on reconnaît bien et modifie le taux d'erreur en attribut
 
 	std::vector <Layer> m_layers;
 	double m_error;
--- Programme/Programme/main.cpp
+++ Programme/Programme/main.cpp
@@ -3,10 +3,39 @@
 #include <string>
 #include <vector>
 #include "extract_data.h"
+#include "Network.h"
 #include "EasyBMP.h" // external library using namespace std;
 
 int main(){
-	data test0("\\Users\\Bouxtehouve\\Dropbox\\C++ Neural Network\\data");
+	//data test0("\\Users\\Bouxtehouve\\Dropbox\\C++ Neural Network\\data");
 	// direction path, with double \\;
-	cout << test0.output_bmp(1258)<< endl;
+	//cout << test0.output_bmp(1258)<< endl;
+	std::vector<unsigned> archi;
+	cout << "cc1" << endl;
+	archi.push_back(2);
+	cout << "cc2" << endl;
+	archi.push_back(2);
+	cout << "cc3" << endl;
+	archi.push_back(1);
+	cout << "cc4" << endl;
+	double eta = 0.5;
+	cout << "cc5" << endl;
+	double alpha = 0.5;
+	cout << "cc6" << endl;
+	Network rdn(eta, alpha, TAN, archi);
+	cout << "cc7" << endl;
+	std::vector<double> input(2);
+	cout << "cc8" << endl;
+	input.push_back(3.5);
+	cout << "cc9" << endl;
+	input.push_back(2.9);
+	cout << "cc10" << endl;
+	std::vector<double> output(1);
+	cout << "cc11" << endl;
+	output.push_back(6.7);
+	cout << "cc12" << endl;
+	rdn.feedForwardNetwork(input);
+	cout << "cc13" << endl;
+	rdn.singleTraining(input, output);
+	cout << "cc14" << endl;
 }
